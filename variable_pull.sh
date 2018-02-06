a739f243fe288b1e0cf3093e188b2f6ceb9697c3
Edward Nunez <Edward.Nunez@cyberark.com>
2018-02-05 18:20:35 -0800

    rolled back to printf

--- variable_pull.sh
+++ variable_pull.sh
@@ -6,12 +6,12 @@ function main() {
 
 function variable_pull(){
 	local api=$(cat ~/.netrc | grep password | awk '$1=="password"{print $2}')
-	echo "This is the API key = $api\n"
+	printf "This is the API key = $api\n"
 	local auth_token=$(curl -s -k -H "Content-Type: text/plain" -X POST -d "$api" https://conjur-master/api/authn/users/admin/authenticate)
 	local token=$(echo -n $auth_token | base64 | tr -d '\r\n')
-	echo "\nThe token is: $token\n"
+	printf "\nThe token is: $token\n"
 	local git_username=$(curl -s -k -X GET -H "Authorization: Token token=\"$token\"" https://conjur-master/api/variables/Foundation%2Fjenkins%2Fgit_username/value?)
-	echo "The Variable Value is: $git_username\n"
+	printf "The Variable Value is: $git_username\n"
 }
 
 main
