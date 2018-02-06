bcfe9fc16139f24d4992fa1e8a2cce81f037d60f
Edward Nunez <Edward.Nunez@cyberark.com>
2018-02-05 17:30:32 -0800

    Fixed.
    os.environment => os.environ

--- pythonAccess.py
+++ pythonAccess.py
@@ -4,7 +4,7 @@ import os
 
 access_key = os.environ['AWS_ACCESS_KEY_ID']
 secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
-git_username = os.environment['GIT_USERNAME']
+git_username = os.environ['GIT_USERNAME']
 
 message = "{\n   AccessKeyID : " + access_key + ",\n   SecretAccessKey : " + secret_key + "\n   GITUserName : " + git_username + "\n}"
 
